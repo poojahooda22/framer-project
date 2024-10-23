f9e7c669b94e65ba4a03209ceba7d3738cffc0bf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-23 14:35:40 +0530

    updated

--- src/AnimatePage.tsx
+++ src/AnimatePage.tsx
@@ -6,39 +6,39 @@ interface AnimatedPageProps {
     children: React.ReactNode;
 }
 
-// const animations = {
-//   initial: { opacity: 0, x: 100 },
-//   animate: { opacity: 1, x: 0 },
-//   exit: { opacity: 0, x: -100 },
-// };
+const animations = {
+  initial: { opacity: 0, x: 100 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: -100 },
+};
 
 
  
 const AnimatePage = ({ children }: AnimatedPageProps) => {
-    const [isPresent, safeToRemove] = usePresence();
-    const [scope, animate] = useAnimate();
+    // const [isPresent, safeToRemove] = usePresence();
+    // const [scope, animate] = useAnimate();
 
-    useEffect(() => {
-        if(!isPresent) {
-            const exitAnimation = async () => {
-                await animate(scope.current, {
-                    opacity: 0,
-                    x: 240
-                },
-            {
-                ease: 'easeInOut',
-                duration: 1
-            })
+    // useEffect(() => {
+    //     if(!isPresent) {
+    //         const exitAnimation = async () => {
+    //             await animate(scope.current, {
+    //                 opacity: 0,
+    //                 x: 240
+    //             },
+    //         {
+    //             ease: 'easeInOut',
+    //             duration: 1
+    //         })
 
-            safeToRemove();
-            }
-            exitAnimation();
-        }
-    }, [isPresent]);
+    //         safeToRemove();
+    //         }
+    //         exitAnimation();
+    //     }
+    // }, [isPresent]);
     
     return (
         <motion.div
-            ref={scope}
+            variants={animations}
             transition={{ duration: 1 , ease: 'easeInOut'}}
         >
             {children}
