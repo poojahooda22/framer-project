e86c576d47d2de8899d12b7a9e5662ea58a60f19
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-11-03 18:06:56 +0530

    import logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { RxHamburgerMenu } from "react-icons/rx";
+import logo from '../../assets/logo.png'
 
 const data ={
     links: [
@@ -29,7 +30,7 @@ const Navbar = () => {
   return (
     <div className="container mx-auto py-2 flex items-center justify-between">
         <div className="w-[4vw]">
-            <img src="/src/assets/logo.png" />
+            <img src={logo} />
         </div>
         <div className=" space-x-[3.5vw] text-[1vw] font-medium text-gray-700 uppercase">
             {data.links.map((link,index) => {
